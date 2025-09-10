10eaf9495cd748e4fe29a507192002fc22345244
serikawa sota <bb54124626@ms.nagasaki-u.ac.jp>
2025-09-10 17:39:54 +0900

    __init__.pyに自身のファイルの役割をコメントアウトで記述した。

--- __init__.py
+++ __init__.py
@@ -1,2 +1,4 @@
+# __init__.pyがあるディレクトリはPythonパッケージとして認識されるようになる。
+# spatialcvディレクトリ内に__init__.pyがあるので, spatialcvを読み込める！
 from .spatial_train_test_split import spatial_train_test_split, __version__
 __all__ = ["spatial_train_test_split", "__version__"]
