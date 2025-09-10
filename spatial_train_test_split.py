e5ea6f2d0389e8a19a83324bfe89aae3701029da
serikawa sota <bb54124626@ms.nagasaki-u.ac.jp>
2025-09-10 15:40:18 +0900

    visualize_train_test_blockのarchiveへの移行

--- spatial_train_test_split.py
+++ spatial_train_test_split.py
@@ -30,7 +30,7 @@ import matplotlib.pyplot as plt
 from sklearn.utils import resample
 from matplotlib.colors import ListedColormap, to_rgba
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 def spatial_train_test_split(
     X,
@@ -295,47 +295,7 @@ def spatial_train_test_split_v4(
 
 
 
-def visualize_train_test_blocks(
-    y,
-    train_mask,
-    test_mask,
-    background_label = 0,
-    title = "Spatial Trian/Test Split Visualization"
-):
-    """
-    教師データ・テストデータの空間配置をオーバーレイ表示する関数
 
-    <動作状況>
-    教師データ・テストデータの空間配置を可視化できる。
-    Args:
-        y (ndarray): ラベルマップ (H, W)
-        train_mask (ndarray): 教師データのマスク (H, W)
-        test_mask (ndarray): テストデータのマスク (H, W)
-        background_label (int): 背景クラスラベル
-        title (str, optional): グラフタイトル
-    """
-
-    # 可視化用配列作成: 背景はnan, train = 1, test = 2
-    display = np.full(y.shape, np.nan, dtype = float)
-    display[train_mask] = 1
-    display[test_mask] = 2
-
-    # カラーマップを定義(train = 青, test = 赤)
-    cmap = ListedColormap(["#1f77b4","#d62728"])
-
-    # ブロット設定
-    plt.figure(figsize = (7,7))
-    im = plt.imshow(display, cmap = cmap, interpolation = "none")
-    plt.title(
-        f"{title}\nTrain={np.sum(train_mask)} / Test={np.sum(test_mask)}",
-        fontsize = 12
-    )
-    plt.axis("off")
-
-    # 凡例設定
-    cbar = plt.colorbar(im, ticks = [1,2])
-    cbar.ax.set_yticklabels(["Train","Test"])
-    plt.show()
 
 def visualize_train_test_with_counts(
     y,
